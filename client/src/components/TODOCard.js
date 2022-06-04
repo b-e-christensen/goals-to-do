0a9e4001ccb23a1720975dbc7078711eed906ced
Ben Christensen <ben.eric.christensen@gmail.com>
2022-06-04 12:58:45 -0600

    functionality added to dos page

--- client/src/components/TODOCard.js
+++ client/src/components/TODOCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 import { Container, Card } from 'react-bootstrap';
 import { GET_USER_ALL } from '../utils/queries'
@@ -9,38 +9,80 @@ const TODOCard = (props) => {
   const [removeTodo] = useMutation(REMOVE_TODO);
 
   const { loading, data, refetch } = useQuery(GET_USER_ALL);
+
+  useEffect(() => {
+    refetch()
+  })
+
   const userInfo = data?.getUser.todos || [];
+
+  const [viewState, setViewState] = useState('incomplete')
 
 
   return (
     <Container className='custom-cont-alter' data-count={props.count}>
-      <h2>
-        {userInfo.length
-          ? `Viewing ${userInfo.length} To Do's:`
-          : 'Create a To Do'}
-      </h2>
+    {/* initial render will display incomplete to dos */}
+      {viewState ? (
+        <>
+          <div className='display-flex justify-space-between'>
+            <h2>
+              {userInfo.length
+                ? `Viewing Incomplete To Do's:`
+                : 'Create a To Do'}
+            </h2>
+            <button className='w-fit-content h-fit-content' onClick={() => { setViewState('') }}>View Complete To Do's</button>
+          </div>
+          {userInfo.map((todo) => {
+            return (
+              todo.completed ? ('') : 
+              (<Card key={todo._id} border='dark' className='custom-card-width'>
+                  <Card.Body id={todo._id}>
+                    <Card.Title>{todo.name}</Card.Title>
+                    <p className='small'>Priority: {todo.priority}</p>
+                    {todo.completed ? (<Card.Text>Completed</Card.Text>) : (
+                      <Card.Text>Not yet completed</Card.Text>
+                    )}
 
-      {userInfo.map((todo) => {
-        return (
-          <Card key={todo._id} border='dark' className='custom-card-width'>
-            <Card.Body id={todo._id}>
-              <Card.Title>{todo.name}</Card.Title>
-              <p className='small'>Priority: {todo.priority}</p>
-              {todo.completed ? (<Card.Text>Completed</Card.Text>) : (
-                <Card.Text>Not yet completed</Card.Text>
-              )}
-              
-              <label> Mark as Complete
-                <input type="checkbox" onChange={(e) => {updateTodo({variables: {_id: todo._id, name: todo.name, completed: true, priority: todo.priority}}); refetch()}}/>
-              </label>
-              <label> Remove
-                <input type="checkbox" onChange={(e) => { removeTodo({variables: {_id: todo._id}}); refetch() }} />
-              </label>
-            </Card.Body >
-          </Card>
-        );
-      })}
+                    <label> Mark as Complete
+                      <input type="checkbox" onChange={(e) => { updateTodo({ variables: { _id: todo._id, name: todo.name, completed: true, priority: todo.priority } }); refetch() }} />
+                    </label>
+                    <label> Remove
+                      <input type="checkbox" onChange={(e) => { removeTodo({ variables: { _id: todo._id } }); refetch() }} />
+                    </label>
+                  </Card.Body>
+                </Card>))
+          })}
+        </>) : (<>
+          <div className='display-flex justify-space-between'>
+            <h2>
+              {userInfo.length
+                ? `Viewing Incomplete To Do's:`
+                : 'Create a To Do'}
+            </h2>
+            <button className='w-fit-content' onClick={() => { setViewState('incomplete') }}>View Incomplete To Do's</button>
+          </div>
+          {userInfo.map((todo) => {
+            return (
+              todo.completed ? (
+                <Card key={todo._id} border='dark' className='custom-card-width'>
+                  <Card.Body id={todo._id}>
+                    <Card.Title>{todo.name}</Card.Title>
+                    <p className='small'>Priority: {todo.priority}</p>
+                    {todo.completed ? (<Card.Text>Completed</Card.Text>) : (
+                      <Card.Text>Not yet completed</Card.Text>
+                    )}
 
+                    <label> Mark as Complete
+                      <input type="checkbox" onChange={(e) => { updateTodo({ variables: { _id: todo._id, name: todo.name, completed: true, priority: todo.priority } }); refetch() }} />
+                    </label>
+                    <label> Remove
+                      <input type="checkbox" onChange={(e) => { removeTodo({ variables: { _id: todo._id } }); refetch() }} />
+                    </label>
+                  </Card.Body>
+                </Card>) : ('')
+            );
+          })}
+        </>)}
     </Container>
   );
 };
