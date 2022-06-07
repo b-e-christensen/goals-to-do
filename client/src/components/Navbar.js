0688ca6a9f00e24f0d3283d71e63d7c767fb14dc
anon123123123 <cditodavis96@gmail.com>
2022-06-06 19:59:03 -0600

    Initi dark mode

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,15 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 import Auth from '../utils/auth';
+import ThemeSVG from '../styles/images/theme.svg'
+import useLocalStorage from 'use-local-storage'
 
 // In Navbar, we can assign a style from an object by using curly braces
 function Navbar({ currentPage, handlePageChange }) {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+  const el = document.getElementById('root')
+  el.classList.add(`${theme}`)
+  
+  const switchTheme = () => {
+    el.classList.remove(`${theme}`)
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme)
+  }
+
+
+
+  console.log(document.getElementsByTagName('body'))
   return (
     <div className='nav-card'>
       <div className='nav-card-header'>
         <Link to="/todos">To Do</Link>
         <Link to="/goals">Goals</Link>
-        <button onClick={Auth.logout}>Logout</button>
+        <button aria-label="Changes light and dark mode" id="darkmode-button" className="nav-link-dark" onClick={switchTheme}>
+                        <img src={ThemeSVG} className="nav-link-dark-img"></img>
+                        </button>
+        <button onClick={Auth.logout} className="logout-btn">Logout</button>
         </div>
 
     </div>
